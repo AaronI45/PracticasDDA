3e8d4e5feec30bd8cc7d6e4c0b55569505258951
AaronI45 <aaron.145@hotmail.com>
2023-12-07 03:36:39 -0600

    Arreglar aplicaciones

--- Books/app/src/main/java/com/example/books/DetailActivity.java
+++ Books/app/src/main/java/com/example/books/DetailActivity.java
@@ -1,14 +1,31 @@
 package com.example.books;
 
+import android.graphics.Bitmap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.books.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
+
+    public static final String BOOK_KEY = "book";
+    public static final String BITMAP_KEY = "bitmap";
+    private ActivityDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Bundle extras = getIntent().getExtras();
+        Book ratedBook = extras.getParcelable(BOOK_KEY);
+        Bitmap bitmap = extras.getParcelable(BITMAP_KEY);
+
+        if (bitmap != null) {
+            binding.imageView.setImageBitmap(bitmap);
+        }
+        binding.setRatedBook(ratedBook);
     }
+
 }
